d3f80c426591cd17422cf0a08616bdfbdc49b697
Stephen Just <stephenjust@gmail.com>
2015-03-19 00:07:37 -0600

    Remove m-stack, use Microchip's USB HID demo
    m-stack wasn't compatible with my device, but
    the existing Microchip demo code was. So far this
    code is at least properly enabling the usb tranceiver.

---
+++ system.c
@@ -0,0 +1,83 @@
+/********************************************************************
+ Software License Agreement:
+
+ The software supplied herewith by Microchip Technology Incorporated
+ (the "Company") for its PIC(R) Microcontroller is intended and
+ supplied to you, the Company's customer, for use solely and
+ exclusively on Microchip PIC Microcontroller products. The
+ software is owned by the Company and/or its supplier, and is
+ protected under applicable copyright laws. All rights are reserved.
+ Any use in violation of the foregoing restrictions may subject the
+ user to criminal sanctions under applicable laws, as well as to
+ civil liability for the breach of the terms and conditions of this
+ license.
+
+ THIS SOFTWARE IS PROVIDED IN AN "AS IS" CONDITION. NO WARRANTIES,
+ WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT NOT LIMITED
+ TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. THE COMPANY SHALL NOT,
+ IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL OR
+ CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ *******************************************************************/
+
+
+#include <system.h>
+#include <system_config.h>
+#include <usb/usb.h>
+
+/** CONFIGURATION Bits **********************************************/
+
+#pragma config CPUDIV = NOCLKDIV
+#pragma config USBDIV = OFF
+#pragma config FOSC   = HS
+#pragma config PLLEN  = ON
+#pragma config FCMEN  = OFF
+#pragma config IESO   = OFF
+#pragma config PWRTEN = OFF
+#pragma config BOREN  = OFF
+#pragma config BORV   = 30
+#pragma config WDTEN  = OFF
+#pragma config WDTPS  = 32768
+#pragma config MCLRE  = OFF
+#pragma config HFOFST = OFF
+#pragma config STVREN = ON
+#pragma config LVP    = ON
+#pragma config XINST  = OFF
+#pragma config BBSIZ  = OFF
+#pragma config CP0    = OFF
+#pragma config CP1    = OFF
+#pragma config CPB    = OFF
+#pragma config WRT0   = OFF
+#pragma config WRT1   = OFF
+#pragma config WRTB   = OFF
+#pragma config WRTC   = OFF
+#pragma config EBTR0  = OFF
+#pragma config EBTR1  = OFF
+#pragma config EBTRB  = OFF
+
+/*********************************************************************
+* Function: void SYSTEM_Initialize( SYSTEM_STATE state )
+*
+* Overview: Initializes the system.
+*
+* PreCondition: None
+*
+* Input:  SYSTEM_STATE - the state to initialize the system into
+*
+* Output: None
+*
+********************************************************************/
+void SYSTEM_Initialize( SYSTEM_STATE state )
+{
+    switch(state)
+    {
+        case SYSTEM_STATE_USB_START:
+            break;
+            
+        case SYSTEM_STATE_USB_SUSPEND: 
+            break;
+            
+        case SYSTEM_STATE_USB_RESUME:
+            break;
+    }
+}
